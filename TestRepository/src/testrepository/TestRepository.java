e591e93cf66eeb3e97fe6c0dd352626e0ffefb0a
Nekupaska <dan-rex@hotmail.es>
2017-04-29 15:38:27 +0200

    Update TestRepository.java
    Third modification

--- TestRepository/src/testrepository/TestRepository.java
+++ TestRepository/src/testrepository/TestRepository.java
@@ -5,7 +5,7 @@ public class TestRepository {
     public static void main(String[] args) {
         //PERKELE
         for (int i = 0; i < 10; i++) {
-            System.out.println("GitHub");
+            System.out.println("Git Gud fugger");
         }
         
     }
