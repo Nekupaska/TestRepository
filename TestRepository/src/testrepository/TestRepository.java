a6b5e884d4fc5c130b6a537fc3cdd422962d33dd
Nekupaska <Nekupaska@Neku>
2017-04-29 15:36:42 +0200

    Second files uploaded to GitHub

--- TestRepository/src/testrepository/TestRepository.java
+++ TestRepository/src/testrepository/TestRepository.java
@@ -4,6 +4,10 @@ public class TestRepository {
 
     public static void main(String[] args) {
         //PERKELE
+        for (int i = 0; i < 10; i++) {
+            System.out.println("GitHub");
+        }
+        
     }
     
 }
